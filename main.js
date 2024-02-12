1af9b5bdfaf1b54059ed518d115b2d602beb056b
kaylee <151891463+kaylee-k@users.noreply.github.com>
2024-02-11 22:23:22 -0700

    Initial JS and updated CSS, HTML
    Create game field with random carrots and bugs.

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const CARROT_SIZE = 80;
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+
+// Create and add bugs and carrots to the field.
+function initGame() {
+  console.log(fieldRect);
+  addItem('carrot', 5, 'img/carrot.png');
+  addItem('bug', 5, 'img/bug.png');
+}
+
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement('img');
+    item.setAttribute('class', className);
+    item.setAttribute('src', imgPath);
+    item.style.position = 'absolute';
+    const x = randomNumber(x1, x2);
+    const y = randomNumber(y1, y2);
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+    field.appendChild(item);
+  }
+}
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+initGame();
